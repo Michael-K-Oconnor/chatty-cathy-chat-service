a767e2fb5fc0be799e3f9c5be97b1810acb3be8c
Mike O'Connor <m.k.oconnor53@gmail.com>
2019-01-31 10:54:06 -0600

    tries to fix db connection issue

--- knexfile.js
+++ knexfile.js
@@ -1,17 +1,15 @@
 require('dotenv').config();
 
+console.log(process.env.NODE_ENV);
+
 module.exports = {
   development: {
     client: 'postgresql',
     connection: {
-      // database: 'chattycathytest',
-      // user: process.env.DB_USER,
-      // password: process.env.DB_PASS,
-      // host: process.env.DB_HOST
-      database: 'chattycathy',
-      user: 'student',
-      password: 'student',
-      host: 'localhost'
+      database: 'chattycathytest',
+      user: process.env.DB_USER,
+      password: process.env.DB_PASS,
+      host: process.env.DB_HOST
     },
     pool: {
       min: 2,
