57ba1a4d859336326e3fc81b03cacdaf241e41f3
Mike O'Connor <m.k.oconnor53@gmail.com>
2019-02-04 16:16:01 -0600

    updates travis tests

---
+++ scripts/travis-check-version.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+PREV_VERSION=$(curl https://raw.githubusercontent.com/Michael-K-Oconnor/Chatty-Cathy/master/package.json \
+  | grep version \
+  | awk -F: '{ print $2 }' \
+  | sed 's/[",]//g')
+
+CURR_VERSION=$(cat package.json \
+  | grep version \
+  | head -1 \
+  | awk -F: '{ print $2 }' \
+  | sed 's/[",]//g' \
+  | tr -d '[[:space:]]')
+
+
+echo $PREV_VERSION
+echo $CURR_VERSION
+
+if [ $PREV_VERSION != $CURR_VERSION ]
+  then
+    echo "success"
+else
+  echo "fail"
+fi
