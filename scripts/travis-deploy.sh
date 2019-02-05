bc882c548da89c4f2898f0c2c4169d5c091de560
Mike O'Connor <m.k.oconnor53@gmail.com>
2019-02-04 19:46:31 -0600

    one more try

--- scripts/travis-deploy.sh
+++ scripts/travis-deploy.sh
@@ -2,4 +2,4 @@
 
 docker push michaelkoconnor/chatty-cathy
 
-# kubectl set image deployments/chatty-cathy-deployment chatty-cathy=michaelkoconnor/chatty-cathy:latest
+kubectl set image deployments/chatty-cathy-deployment chatty-cathy=michaelkoconnor/chatty-cathy:latest
