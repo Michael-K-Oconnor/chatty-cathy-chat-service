fba3b1a29440c1de944453463185efb13867fefc
Mike O'Connor <m.k.oconnor53@gmail.com>
2019-02-04 20:34:30 -0600

    try again

--- scripts/travis-install-kubectl.sh
+++ scripts/travis-install-kubectl.sh
@@ -4,12 +4,14 @@
 curl -LO https://storage.googleapis.com/kubernetes-release/release/$(curl -s https://storage.googleapis.com/kubernetes-release/release/stable.txt)/bin/linux/amd64/kubectl
 chmod +x ./kubectl
 sudo mv ./kubectl /usr/local/bin/kubectl
- 
+
+curl -o config https://$GITHUB_ACCESS_TOKEN@raw.githubusercontent.com/michael-k-oconnor/chatty-cathy-k8-config/master/
 mkdir ${HOME}/.kube
-cp scripts/k8-config.yml ${HOME}/.kube/config
- 
+cp config ${HOME}/.kube/config
+#cp scripts/k8-config.yml ${HOME}/.kube/config
+ls ${HOME}/.kube/config
 # Fill out missing params in kubectl config file
-kubectl config set clusters.ChattyCathyMain.certificate-authority-data "$CERTIFICATE_AUTHORITY_DATA"
-kubectl config set clusters.ChattyCathyMain.server "$CLUSTER_SERVER"
-kubectl config set users.kubernetes-kube-group-e1ea0b-admin.client-certificate-data "$CLIENT_CERTIFICATE_DATA"
-kubectl config set users.kubernetes-kube-group-e1ea0b-admin.client-key-data "$CLIENT_KEY_DATA"
+# kubectl config set clusters.ChattyCathyMain.certificate-authority-data "$CERTIFICATE_AUTHORITY_DATA"
+# kubectl config set clusters.ChattyCathyMain.server "$CLUSTER_SERVER"
+# kubectl config set users.kubernetes-kube-group-e1ea0b-admin.client-certificate-data "$CLIENT_CERTIFICATE_DATA"
+# kubectl config set users.kubernetes-kube-group-e1ea0b-admin.client-key-data "$CLIENT_KEY_DATA"
