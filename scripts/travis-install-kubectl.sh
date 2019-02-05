ed4df424d277f02ecbb450d092b526160e52ded9
Mike O'Connor <m.k.oconnor53@gmail.com>
2019-02-04 21:03:22 -0600

    travis

--- scripts/travis-install-kubectl.sh
+++ scripts/travis-install-kubectl.sh
@@ -5,11 +5,19 @@ curl -LO https://storage.googleapis.com/kubernetes-release/release/$(curl -s htt
 chmod +x ./kubectl
 sudo mv ./kubectl /usr/local/bin/kubectl
 
-curl -o config https://$GITHUB_ACCESS_TOKEN@raw.githubusercontent.com/michael-k-oconnor/chatty-cathy-k8-config/master/
+
+git clone https://michael-k-oconnor:$GH_PASS@github.com/michael-k-oconnor/chatty-cathy-k8-config.git
 mkdir ${HOME}/.kube
-cp config ${HOME}/.kube/config
+mv ./chatty-cathy-k8-config/* ${HOME}/.kube
+
+ls ${HOME}/.kube
+
+
+
+
+# cp config ${HOME}/.kube/
+# curl -o config https://$GITHUB_ACCESS_TOKEN@raw.githubusercontent.com/michael-k-oconnor/chatty-cathy-k8-config/master/
 #cp scripts/k8-config.yml ${HOME}/.kube/config
-ls ${HOME}/.kube/config
 # Fill out missing params in kubectl config file
 # kubectl config set clusters.ChattyCathyMain.certificate-authority-data "$CERTIFICATE_AUTHORITY_DATA"
 # kubectl config set clusters.ChattyCathyMain.server "$CLUSTER_SERVER"
